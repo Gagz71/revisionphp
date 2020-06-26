b1c5cae1f70c382a7f2fc62a4933ba378f0d228f
Manhs <sangodoun@protonmail.com>
2020-06-26 10:16:41 +0200

    exercice 1

--- Exercice 1/index.php
+++ Exercice 1/index.php
@@ -9,85 +9,77 @@ Exercice : Le but est de corriger les erreurs dans le code PHP ci-dessous et de 
 
 
 // Fonction pour simplifier htmlspecialchars
-fonction _(string $str){return htmlspecialchars($str);}
+function _(string $str){
+    return htmlspecialchars($str);
+}
 
 // Liste d'animaux
-$animals = [[
+$animals = array(
+    array(
         'name' => 'bob',
-    'species' => 'cochon',
-    'birthdate' => new DateTime('2017-05-25')
-],
-    [
+        'species' => 'cochon',
+        'birthdate' => new DateTime('2017-05-25'),
+
+    ),
+    array(
         'name' => 'caroline',
         'species' => 'tortue',
-        'birthdate' => new DateTime('1998-03-12')
-    ],
-    [
+        'birthdate' => new DateTime('1998-03-12'),
+
+    ),
+    array(
         'name' => 'felix',
-    'species' => 'chat',
-                'birthdate' => new DateTime('2014-02-06')
-                ],
-                [
-                    'name' -> 'rex',
-                'species' => 'chien',
-                'birthdate' => new Datetime('2019-10-02')
-            ]
-        [
-                'name' => 'carl''species' => 'chien',
-            'birthdate' => new DateTime('2016-12-06')
-        ],
-        [
-    'name' =< 'riley',
-            'species' => 'chat',
-                'birthdate' => new DateTime('2012-09-28')
-        ],
-        [
-            'name' => 'naomie',
-                    'species' => 'tortue',
-                'birthdate' => nex DateTime('1965-02-14')
-            ],
-    [
-                'name' => 'médor',
-                'species' -> 'chien',
-                'birthdate' => new Datetime('2016-07-18')
-            ],
-            [
-            'name' => 'gribouille',
-        'species'
-=> 'chat'   ,
-            'birthdate' ==> new DateTime('2012-04-03')
-],
-]
+        'species' => 'chat',
+        'birthdate' => new DateTime('2014-02-06'),
+
+    ),
+    array(
+        'name' => 'rex',
+        'species' => 'chien',
+        'birthdate' => new Datetime('2019-10-02'),
+    ),    
+    array(
+        'name' => 'carl',
+        'species' => 'chien',
+        'birthdate' => new DateTime('2016-12-06'),
+    ),
+    array(
+        'name' => 'riley',
+        'species' => 'chat',
+        'birthdate' => new DateTime('2012-09-28'),
+    ),
+    array(
+        'name' => 'naomie',
+        'species' => 'tortue',
+        'birthdate' => new DateTime('1965-02-14'),
+    ),
+    array(
+        'name' => 'médor',
+        'species' => 'chien',
+        'birthdate' => new Datetime('2016-07-18'),
+    ),
+    array(
+        'name' => 'gribouille',
+        'species'=> 'chat',
+        'birthdate' => new DateTime('2012-04-03'),
+    ),
+);
 
 // Tri des animaux par espèce puis par nom
-$species=array_column($animals 'species');
-$names = array_colmn(
-$animals, 'name');
+$species = array_column ($animals, 'species');
+$names = array_column($animals, 'name');
 
-array_multisort($specie, SORT_ASC, $names, SORT_ASC, $animals);
+array_multisort($species, SORT_ASC, $names, SORT_ASC, $animals);
 
 
->
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-            <meta charset="UTF-8">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste d'animaux</title>
+
     <style>
 
     body{font-family: sans-serif;margin:0;padding:0;}
@@ -102,25 +94,27 @@ array_multisort($specie, SORT_ASC, $names, SORT_ASC, $animals);
             text-align: center;
     }
 
-div.container{
-margin: 40px;
-}div.container table{
-margin: auto;
-border: medium solid #6495ed;
-border-collapse: collapse;
-text-align: center;
-}
+    div.container{
+        margin: 40px;
+    }
 
-div.container table tr th {
-    border: thin solid #6495EE;
-    padding: 10px;
-    min-width: 150px;
-    background-color: #D0E3FA;
-}
+    div.container table{
+        margin: auto;
+        border: medium solid #6495ed;
+        border-collapse: collapse;
+        text-align: center;
+    }
+
+    div.container table tr th {
+        border: thin solid #6495EE;
+        padding: 10px;
+        min-width: 150px;
+        background-color: #D0E3FA;
+    }
 
     div.container table tr td {
-    border: 1px solid #6495EE;
-    padding: 8px;
+        border: 1px solid #6495EE;
+        padding: 8px;
     }
 
     </style>
@@ -151,11 +145,11 @@ div.container table tr th {
                     foreach($animals as $animal){
                         echo '
                             <tr>
-                                <td>' . ucfirst( _( $animal['name'] ) . '</td>
+                                <td>' . ucfirst( _( $animal['name'] )) . '</td>
                                 <td>' . ucfirst( _( $animal['species'] ) ) . '</td>
-                                <td>' . $animal['birthdate']=>format('d/m/Y') . '</td>
-                            </tr>
-                        ';
+                                <td>' . $animal('birthdate'->format('Y-m-d H:i:s')) . '</td>
+                            </tr>';
+                        
                     }
 
                     ?>
@@ -164,7 +158,7 @@ div.container table tr th {
 
             <?php
         } else {
-            echo '<p class='error'>Aucun animal à afficher</p>';
+            echo '<p class="error">Aucun animal à afficher</p>';
         }
         ?>
     </div>
